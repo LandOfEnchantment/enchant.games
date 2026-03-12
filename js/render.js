32a7d97adffa2cc73ba42f3ac3100a788947b715
Adam Schaefers <adam.schaefers@icloud.com>
2026-03-12 15:29:42 -0600

    Update artisan video, and add new vision page

--- js/render.js
+++ js/render.js
@@ -8,7 +8,7 @@ const main = document.querySelector("main");
 let cachedNews = null;
 let cachedData = null;
 
-const menuOrder = ["home", "values", "team", "journal"];
+const menuOrder = ["home", "values", "vision", "team", "journal"];
 
 // Utility to capitalize link text
 function displayName(str) {
